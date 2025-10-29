0766314870721d3b4c514d2c1c1e0ba84582cd98
minsu0707 <mandoo4137@gmail.com>
2025-10-29 20:23:08 +0900

    fix: 결과 응답값 이용

--- src/hooks/mutations/use-create-todo-mutation.ts
+++ src/hooks/mutations/use-create-todo-mutation.ts
@@ -1,5 +1,6 @@
 import { createTodo } from "@/api/create-todo";
 import { QUERY_KEYS } from "@/lib/constants";
+import type { Todo } from "@/types";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 export function useCreateTodoMutation() {
@@ -9,13 +10,16 @@ export function useCreateTodoMutation() {
     mutationFn: createTodo,
     onMutate: () => {},
     onSettled: () => {},
-    onSuccess: () => {
+    onSuccess: (newTodo) => {
       /*  todos의 캐시 데이터를 무효화 메서드
       즉, 데이터를 다시 리페칭 시킴 */
-
       // 이렇게 쿼리키를 상수로 선언해서 나누어 사용하면 불필요한 데이터 리페칭 발생 X
-      queryclient.invalidateQueries({
-        queryKey: QUERY_KEYS.todo.list,
+      // queryclient.invalidateQueries({
+      //   queryKey: QUERY_KEYS.todo.list,
+      // });
+      queryclient.setQueryData<Todo[]>(QUERY_KEYS.todo.list, (prevTodos) => {
+        if (!prevTodos) return [newTodo];
+        return [...prevTodos, newTodo];
       });
     },
     onError: (error) => {
